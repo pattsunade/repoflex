de6cd29aaf41f56f4e453b76eca8945742b9ca53
Patricia Delgado <pdelgado@retailpro.cl>
2022-09-14 10:52:27 -0300

    repoflex

---
+++ Repoflex/Shared/RepoflexApp.swift
@@ -0,0 +1,17 @@
+//
+//  RepoflexApp.swift
+//  Shared
+//
+//  Created by Zolbit on 12-09-22.
+//
+
+import SwiftUI
+
+@main
+struct RepoflexApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
